a5d4893108ecdfa97328b1014687241ffe041d94
andrewdelgadilloxi <andrew.delgadillo@xifin.com>
2025-06-03 14:26:40 -0700

    Update functions_v8.sh

--- functions_v8.sh
+++ functions_v8.sh
@@ -11,7 +11,7 @@ check_controls_v8() {
   local hostname=$(hostname)
   local timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
   local report_name="${hostname}_tomcat8_cis_compliance_${timestamp}.txt"
-  local report_path="/tmp/$report_name"
+  local report_path="$dir/tomcat8_cis_compliance_report.txt"
 
   echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
   echo "Apache Tomcat 8 Hardening Assessment"
